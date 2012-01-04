e95ff73453f77c69e27e5a16b13fb4075932487d
Bruno <goesmeira@gmail.com>
2012-01-04 00:01:10 -0300

    chapter 3 finished

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views	
 
   describe "GET 'home'" do
     it "returns http success" do
@@ -8,11 +9,42 @@ describe PagesController do
       response.should be_success
     end
   end
+  
+  describe "GET 'home'" do
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+      				    :content=>" | Home")
+    end
+  end
 
   describe "GET 'contact'" do
     it "returns http success" do
       get 'contact'
       response.should be_success
+    end
+  end
+  
+  describe "GET 'contact'" do
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",
+      				    :content=>" | Contact")
+    end
+  end
+  
+  describe "GET 'about'" do
+    it "returns http success" do
+    	get 'about'
+    	response.should be_success
+    end
+  end
+  
+  describe "GET 'about'" do
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+      				    :content=>" | About")
     end
   end
 
